dd3fb2204a879ba3320970d2ca18548adec44d40
Bran <archer0615@gmail.com>
2018-02-07 14:41:38 +0800

    Add WebAPI project

---
+++ MyNoteProject/WebAPI_Note/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAPI_Note.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ MyNoteProject/WebAPI_Note/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPI_Note.WebApiApplication" Language="C#" %>
