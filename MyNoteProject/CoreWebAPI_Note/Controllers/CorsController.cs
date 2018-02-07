8f0f372289f2478cfd14e44a14b52e9ab608142d
Bran <archer0615@gmail.com>
2018-02-07 15:29:24 +0800

    跨 Domain

---
+++ MyNoteProject/CoreWebAPI_Note/Controllers/CorsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebAPI_Note.Controllers
+{
+    public class CorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MyNoteProject/WebAPI_Note/Controllers/HomeController.cs
+++ MyNoteProject/WebAPI_Note/Controllers/HomeController.cs
@@ -11,8 +11,22 @@ namespace WebAPI_Note.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-
+            GetResponse();
             return View();
+        }
+
+        private HttpResponseBase GetResponse()
+        {
+            Response.Headers.Remove("Access-Control-Allow-Origin");
+            Response.AddHeader("Access-Control-Allow-Origin", Request.UrlReferrer.GetLeftPart(UriPartial.Authority));
+
+            Response.Headers.Remove("Access-Control-Allow-Credentials");
+            Response.AddHeader("Access-Control-Allow-Credentials", "true");
+
+            Response.Headers.Remove("Access-Control-Allow-Methods");
+            Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+            return Response;
         }
     }
 }
