238b438ead4a067714e684c0e75441f2706c02fb
Bran <archer0615@gmail.com>
2018-03-01 13:52:07 +0800

    整理

--- MyNoteProject/C_SharpNote/Program.cs
+++ MyNoteProject/C_SharpNote/Program.cs
@@ -12,8 +12,39 @@ namespace C_SharpNote
     {
         static void Main(string[] args)
         {
-            var t = new MyCompare();
-            t.RunProcess();
+            var t = new Task<int>(() =>
+             {
+                 var x = 0;
+                 for (int i = 0; i < 100; i++)
+                 {
+                     x++;
+                 }
+                 return x;
+             });
+            var t2 = new Task<int>(() =>
+            {
+                var y = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    y++;
+                }
+                return y;
+            });
+            //t.Start();
+            var t3 =new Task(() =>
+            {
+                t.Start();
+                t2.Start();
+                if (t2.Status == TaskStatus.RanToCompletion) Console.WriteLine(t2.Result);
+                if (t.Status == TaskStatus.RanToCompletion) Console.WriteLine(t.Result);
+            });
+            t3.Start();
+
+            Console.ReadLine();
         }
+        //public int GetData()
+        //{
+
+        //}
     }
 }
---
+++ MyNoteProject/C_SharpNote/Reflection2_SQL/ReflectionProgram.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SharpNote.Reflection2_SQL
+{
+    public class ReflectionProgram : IProcess   
+    {
+        public void RunMain()
+        {
+            //使用方式
+            ViewModel model = new ViewModel
+            {
+                ID = 1,
+                Name = "Name",
+                Address = "Address",
+                Phone = "12345678"
+            };
+            var spParams = getPropParameters(model);
+            //var result = db.Database
+            //    .ExecuteSqlCommand($"EXEC SP_Name {spParams.Item1}", spParams.Item2);
+        }
+
+        public class ViewModel
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public string Phone { get; set; }
+        }
+
+        protected static Tuple<string, SqlParameter[]> getPropParameters<T>(T model, bool inclNullValParam = false, string[] exclPropNames = null) where T : class
+        {
+            List<SqlParameter> spParams = new List<SqlParameter>();
+            string paramList = "";
+            //取得所有屬性
+            var properties = model.GetType().GetProperties();
+            if (exclPropNames != null)
+                //將所有屬性的名稱裝入Array
+                properties = properties.Where(x => !exclPropNames.Contains(x.Name)).ToArray();
+            //各別裝值
+            foreach (var pi in properties)
+            {
+                string pn = $"@{pi.Name}";
+                var pv = pi.GetValue(model, null);
+                if (inclNullValParam || pv != null)
+                {
+                    paramList += paramList == "" ? pn : $", {pn}";
+                    spParams.Add(new SqlParameter(pn, pv ?? DBNull.Value));
+                }
+            }
+            return Tuple.Create(paramList, spParams.ToArray());
+        }
+    }
+}
